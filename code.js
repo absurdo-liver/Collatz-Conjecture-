374f013222484e20be96a2b1f86765f5d8bff9d5
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-04-17 12:01:38 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,19 @@
+function conjecture(n) { //all dat math shii
+  let result = n + '\n'; // Start with the initial number
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n = n * 3 + 1;
+    }
+    result += n + '\n'; // Append each number
+  }
+  result += 'conjecture finished!';
+  return result; //return results
+}
+
+function start() { //function ran by start button
+  const userinput = parseInt(document.getElementById('userin').value); //parse user input
+  const output = document.getElementById('textelem');
+  output.textContent = conjecture(userinput); //set text on webpage to the string previously appended to
+}
